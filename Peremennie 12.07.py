91baa1f33fd25ecf8030796133603357b8b74ea3
vvolik777 <3443696@mail.ru>
2024-07-12 15:12:22 +0300

    Add files via upload

---
+++ Peremennie 12.07.py
@@ -0,0 +1,5 @@
+homework = 12
+worktime = 1.5
+coursename = 'Python'
+time1work = worktime/homework
+print('Курс:'+str(coursename)+', всего задач:'+str(homework)+', затрачено часов:'+str(worktime)+', среднее время выполнения '+str(time1work)+' часа.')
