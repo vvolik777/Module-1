af03fde798ccbf958506a73fd50af8aebccbdf7b
vasilisa <3443696@mail.ru>
2024-09-09 17:34:09 +0300

    метод `eat` перенесен в род.класс

--- Module-6/Наследование.py
+++ Module-6/Наследование.py
@@ -4,6 +4,14 @@ class Animal:
         self.fed = False  # не накормлено
         self.name = name
 
+    def eat(self, food):
+        if food.edible:
+            print(f"{self.name} съел {food.name}")
+            self.fed = True
+        else:
+            print(f"{self.name} не стал есть {food.name}")
+            self.alive = False
+
 
 class Plant:
     def __init__(self, name):
@@ -12,26 +20,11 @@ class Plant:
 
 
 class Mammal(Animal):
-    def eat(self, food):
-
-        if food.edible:
-            print(f"{self.name} съел {food.name}")
-            self.fed = True
-        else:
-            print(f"{self.name} не стал есть {food.name}")
-            self.alive = False
+    pass
 
 
 class Predator(Animal):
-    def eat(self, food):
-
-        if food.edible:
-
-            print(f"{self.name} съел {food.name}")
-            self.fed = True
-        else:
-            print(f"{self.name} не стал есть {food.name}")
-            self.alive = False
+    pass
 
 
 class Flower(Plant):
