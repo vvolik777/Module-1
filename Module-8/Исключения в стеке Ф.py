2bef3b4677c98d8c0ebf8c1d98f543fe5238a188
vasilisa <3443696@mail.ru>
2024-09-28 17:18:08 +0300

    .

---
+++ Module-8/Исключения в стеке Ф.py
@@ -0,0 +1,30 @@
+def personal_sum(numbers):
+    result = 0
+    incorrect_data = 0
+
+    for item in numbers:
+        try:
+            result += item  # пытаемся добавить элемент к сумме
+        except TypeError:
+            print(f'Некорректный тип данных для подсчёта суммы - {item}')
+            incorrect_data += 1  # увеличиваем счётчик некорректных данных
+
+    return result, incorrect_data
+
+
+def calculate_average(numbers):
+    try:
+        total_sum, incorrect_data = personal_sum(numbers)
+        count = len(numbers) - incorrect_data
+        return total_sum / count
+    except ZeroDivisionError:
+        return 0
+    except TypeError:
+        print('В numbers записан некорректный тип данных')
+        return None
+
+
+print(f'Результат 1: {calculate_average("1, 2, 3")}')
+print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}')
+print(f'Результат 3: {calculate_average(567)}')
+print(f'Результат 4: {calculate_average([42, 15, 36, 13])}')
