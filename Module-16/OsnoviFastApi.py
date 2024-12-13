5ef8d6e147fe83ef546f9487bb5c0c244acd406b
vasilisa <3443696@mail.ru>
2024-12-13 15:27:59 +0300

    .

--- Module-16/OsnoviFastApi.py
+++ Module-16/OsnoviFastApi.py
@@ -7,19 +7,19 @@ app = FastAPI()
 
 @app.get('/')
 async def welcome():
-    return {"message": "Главная страница"}
+    return  "Главная страница"
 
 
 @app.get('/user/admin')
 async def admin():
-    return {"message": "Вы вошли как администратор"}
+    return  "Вы вошли как администратор"
 
 
 @app.get('/user/{user_id}')
 async def user(user_id: int):
-    return {"message": f"Вы вошли как пользователь № {user_id}"}
+    return  f"Вы вошли как пользователь № {user_id}"
 
 
 @app.get('/user')
 async def user_info(username: Optional[str] = "Гость", age: Optional[int] = 0):
-    return {"message": f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
+    return f"Информация о пользователе. Имя: {username}, Возраст: {age}"
