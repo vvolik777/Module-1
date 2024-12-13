bad6238cf10b8936a002e7246ea7dcf0feb4fca1
vasilisa <3443696@mail.ru>
2024-12-13 12:41:25 +0300

    .

---
+++ Module-16/Validation.py
@@ -0,0 +1,24 @@
+from typing import Optional
+from fastapi import FastAPI, Path
+from typing_extensions import Annotated
+
+app = FastAPI()
+
+@app.get('/')
+async def welcome():
+    return {"message": "Главная страница"}
+
+@app.get('/user/admin')
+async def admin():
+    return {"message": "Вы вошли как администратор"}
+
+@app.get('/user/{user_id}')
+async def user(user_id: Annotated[int, Path(title="Enter User ID", ge=1, le=100, examples={"example": 1})]):
+    return {"message": f"Вы вошли как пользователь № {user_id}"}
+
+@app.get('/user/{username}/{age}')
+async def user_info(
+    username: Annotated[str, Path(title="Enter username", min_length=5, max_length=20, examples={"example": "UrbanUser"})],
+    age: Annotated[int, Path(title="Enter age", ge=18, le=120, examples={"example": 24})]
+):
+    return {"message": f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
